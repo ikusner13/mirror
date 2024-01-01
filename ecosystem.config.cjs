622e37c09dc06829008077cbf515df8fb917cd90
ikusner13 <ikusner13@gmail.com>
2023-12-31 21:35:48 -0500

    cron

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   apps: [
     {
+      cron_restart: "0 0 * * *",
       env: {
         NODE_ENV: "production",
       },
+      instances: 1,
       name: "server",
       script: "./build/index.js",
     },
@@ -12,6 +14,7 @@ module.exports = {
       env: {
         DISPLAY: ":0",
       },
+      instances: 1,
       name: "electron-app",
       script: "electron",
     },
