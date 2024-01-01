2e2958b3242a140d1a7e15873b7b8e72837568e0
ikusner13 <ikusner13@gmail.com>
2023-12-31 21:29:14 -0500

    envs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,17 @@
 module.exports = {
   apps: [
     {
+      env: {
+        NODE_ENV: "production",
+      },
       name: "server",
       script: "./build/index.js",
     },
     {
       args: "./main.mjs",
+      env: {
+        DISPLAY: ":0",
+      },
       name: "electron-app",
       script: "electron",
     },
